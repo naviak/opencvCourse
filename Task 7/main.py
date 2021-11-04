ca098ee71b9c2f640b3b1a47c73f3ee40059f02f
naviak <m.dudar@g.nsu.ru>
2021-11-04 08:16:56 +0300

    noise :)

--- Task 7/main.py
+++ Task 7/main.py
@@ -3,6 +3,17 @@ import numpy as np
 import random
 
 
+def create_noise_img(img, param, uniform: bool = True):
+    noise = np.zeros(img.shape)
+    if uniform:
+        cv2.randu(noise, -param, param)
+    else:
+        cv2.randn(noise, 0, param)
+    result = np.clip(cv2.add(np.float64(img), noise), 0, 255)
+
+    return np.uint8(result)
+
+
 def putTextImage(im, text):
     font = cv2.FONT_HERSHEY_SIMPLEX
     bottomLeftCornerOfText = (10, 400)
@@ -19,23 +30,25 @@ def putTextImage(im, text):
 
 
 image = cv2.imread("lena.jpg")
-image = cv2.resize(image,(400,400))
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.resize(image, (400, 400))
+noised = create_noise_img(image, 70, True)
 
-gaus_blur = cv2.blur(image, (3, 3))
-median_blur = cv2.medianBlur(image, 3)
+gaus_blur = cv2.blur(noised, (3, 3))
+median_blur = cv2.medianBlur(noised, 3)
 
 kernel = np.array([[0.0, -1.0, 0.0],
-                   [-1.0, 5.0, -1.0],
-                   [0.0, -1.0, 0.0]])
+                   [8.0, 5.0, -1.0],
+                   [1.0, -1.0, 0.0]])
 kernel = kernel / np.sum(kernel)
-ker_filter = cv2.filter2D(image, -1, kernel)
-sobelx = cv2.Sobel(image, cv2.CV_8U, 1, 0, ksize=3)
-sobely = cv2.Sobel(image, cv2.CV_8U, 0, 1, ksize=3)
-sobelxy = cv2.Sobel(image, cv2.CV_8U, 1, 1, ksize=3)
+ker_filter = cv2.filter2D(noised, -1, kernel)
+sobelx = cv2.Sobel(noised, cv2.CV_8U, 1, 0, ksize=3)
+sobely = cv2.Sobel(noised, cv2.CV_8U, 0, 1, ksize=3)
+sobelxy = cv2.Sobel(noised, cv2.CV_8U, 1, 1, ksize=3)
 
 laplas = cv2.Laplacian(image, cv2.CV_8U, ksize=3)
 
-transforms = {"orig": image, "gaus_blur": gaus_blur, "medianBlur": median_blur,
+transforms = {"orig": noised, "gaus_blur": gaus_blur, "medianBlur": median_blur,
               "filter": ker_filter, "sobelx": sobelx, "sobely": sobely,
               "sobelxy": sobelxy, "laplas": laplas}
 
@@ -49,7 +62,7 @@ print(len(transforms))
 h = np.hstack(tuple(d1.values()))
 l = np.hstack(tuple(d2.values()))
 res = np.vstack((h, l))
-cv2.imshow('key', res)
+cv2.imshow('UNIFORM NOISE', res)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
